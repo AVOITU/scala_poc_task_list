9f470344be7e5cf94af7c3a23394a77454ccece5
Antoine Voituron <avoitu@orange.fr>
2024-10-02 23:47:21 +0200

    first commit and attempts, hello world + begin BO

---
+++ src/main/BO/Task.scala
@@ -0,0 +1,8 @@
+import java.time.LocalDate
+case class Task (id: Integer,
+                name: String,
+                description: String,
+                startingDate: LocalDate,
+                endDate: LocalDate){
+   
+}
